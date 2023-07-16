a342aad05b90ea1dac2dac0de8e81fc5ae0b704a
Aldrineyana <112174796+YAPZORR@users.noreply.github.com>
2023-07-16 21:53:17 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,42 @@
+<header class="cd-header">
+     
+    <div class="header-wrapper">
+        <div class="logo-wrap">
+            <a href="#" class="hover-target"><span>ald</span> <span class="rin">rin</span></a>
+        </div>
+        <div class="nav-but-wrap">
+            <div class="menu-icon hover-target">
+                <span class="menu-icon__line menu-icon__line-left"></span>
+                <span class="menu-icon__line"></span>
+                <span class="menu-icon__line menu-icon__line-right"></span>
+            </div>					
+        </div>					
+    </div>				
+</header>
+
+<div class="nav">
+    <div class="nav__content">
+        <ul class="nav__list">
+            <li class="nav__list-item active-nav"><a href="#" class="hover-target">home</a></li>
+            <li class="nav__list-item"><a href="about.php" class="hover-target">About</a></li>
+            <li class="nav__list-item"><a href="#" class="hover-target">news</a></li>
+            <li class="nav__list-item"><a href="#" class="hover-target">contact</a></li>
+        </ul>
+        <hr>
+        
+    <div class="row">
+        <div class="col-4 nav__list-item"><h5 class="foot">Aldrin Eyana <br> <em >09518189613</em></h5>	 </div>
+            <div class="col-4 nav__list-item" ><h5 >agusan del sur <br> <em>sanfrancisco</em> </h5> </div>
+            
+            <div class="col-4  nav__list-item">
+                <a href=""><i class="fa-brands fa-facebook"></i></a>
+            </div>
+        
+
+    </div>
+            
+    
+    </div>
+
+    
+</div>
